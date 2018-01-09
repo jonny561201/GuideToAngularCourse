119419e93f59338a1e9ab40fd5bb6486728e0cdc
Jonathon Graf <jgraf@pillartechnology.com>
2018-01-08 20:34:20 -0600

    updated to use two way data binding for servers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
-import {ServerComponent} from './server/server.component';
+import { FormsModule } from '@angular/forms';
+import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './successAlert/successAlert.component';
-import {WarningAlertComponent} from './warningAlert/warningAlert.component';
+import { WarningAlertComponent } from './warningAlert/warningAlert.component';
 
 
 @NgModule({
