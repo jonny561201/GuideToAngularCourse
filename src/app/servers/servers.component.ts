2335d6702bb07b4acd29a0d7898b510cc5222055
Jonathon Graf <jgraf@pillartechnology.com>
2018-01-08 18:26:29 -0600

    exemplified how to add inline html to components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <div>you can add inline html this way</div>
+    <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
