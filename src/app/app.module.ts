aa016d7f064f6b39c00d358f55d05a75bbb14a0f
Jonathon Graf <jgraf@pillartechnology.com>
2018-01-08 19:35:21 -0600

    Assignment 1: Create Success and Warning Alerts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './successAlert/successAlert.component';
+import {WarningAlertComponent} from './warningAlert/warningAlert.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
