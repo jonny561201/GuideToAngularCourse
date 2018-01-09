9daa295216db8ed386219957523a8833cc81f66b
Jonathon Graf <jgraf@pillartechnology.com>
2018-01-08 20:12:00 -0600

    added property binding to server components for displaying button

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,15 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <!--<div>you can add inline html this way</div>-->
-    <app-server></app-server>`,
-  styleUrls: ['./servers.component.css']
+  templateUrl: './servers.component.html'
+  // template: `
+  //   <app-server></app-server>
+  //   <!--<div>you can add inline html this way</div>-->
+  //   <app-server></app-server>`,
+  // styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  public allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
